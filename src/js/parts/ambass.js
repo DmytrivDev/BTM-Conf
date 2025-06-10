187a13964cde09e46c739c7aebabdc21991f2dfc
Uzurch1k <galcenkovlad7@gmail.com>
2025-06-10 16:48:10 +0300

    fix evidn modal

--- src/js/parts/ambass.js
+++ src/js/parts/ambass.js
@@ -7,7 +7,7 @@ export function addDataAmbass(modal, event) {
     ava: evidnCard.querySelector('.evidn-ava img')?.src || '',
     name: evidnCard.querySelector('.evidn-name')?.textContent || '',
     activity: evidnCard.querySelector('.evidn-activity p')?.textContent || '',
-    desc: evidnCard.querySelector('.evidn-desc p')?.textContent || '',
+    desc: evidnCard.querySelector('.evidn-desc')?.innerHTML || '',
     soc: evidnCard.querySelector('.evidn-soc')?.innerHTML || '',
     connect: evidnCard.querySelector('.evidn-link')?.href || '#',
   };
@@ -15,7 +15,7 @@ export function addDataAmbass(modal, event) {
   modal.querySelector('.evidence__ava img').src = evidnData.ava;
   modal.querySelector('.evidence__tl').textContent = evidnData.name;
   modal.querySelector('.evidence__activity p').textContent = evidnData.activity;
-  modal.querySelector('.txt-def p').textContent = evidnData.desc;
+  modal.querySelector('.evidence__desc').innerHTML = evidnData.desc;
   modal.querySelector('.evidence__soc').innerHTML = evidnData.soc;
   modal
     .querySelector('.evidence__connect')
